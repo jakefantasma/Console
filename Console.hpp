9c9c8c82b96b17b626a6ff8e1c289153f3b05bef
jake <carloscaballeros321@gmail.com>
2022-10-30 14:14:53 -0600

    se agregan metodos para int y char

--- Console.hpp
+++ Console.hpp
@@ -15,31 +15,55 @@ private:
 
 public:
     IMP(){};
-
     void setColor(int color)
     {
         SetConsoleTextAttribute(CMD_HANDLER, color);
     }
-    void log(std::string msg)
+    // main write log
+    void log(std::string msg) // string
     {
         std::cout << msg << std::endl;
         setColor(__default);
     };
-    void log(int msg)
+    void log(int msg) // int
     {
         log(std::to_string(msg));
     };
-    void error(std::string msg)
+    void log(char msg) // char
+    {
+        std::string s(1, msg);
+        log(s);
+    };
+    // main write error
+    void error(std::string msg) // string
     {
         setColor(__error);
         log(msg);
     };
-    void warning(std::string msg)
+    void error(int msg) // string
+    {
+        error(std::to_string(msg));
+    }
+    void error(char msg) // char
+    {
+        std::string s(1, msg);
+        error(s);
+    }
+    void warning(std::string msg) // string
     {
         setColor(__warning);
         log(msg);
     };
-
+    void warning(char msg) // char
+    {
+        std::string s(1, msg);
+        warning(s);
+    }
+    void warning(int msg) // int
+    {
+        warning(std::to_string(msg));
+    };
+    // string
     ~IMP();
 };
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 int main()
 {
 
+    unsigned char key = 20;
     Consola->log("log 1");
-    Consola->error("error 1");
-    Consola->warning("warning 1");
-    
+    Consola->log(key);
+    // Consola->error("error 1");
+    // Consola->error(key);
+    // Consola->warning("warning 1");
 }
